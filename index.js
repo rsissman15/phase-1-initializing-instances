fd33c7d8ee5da5831fb9ddd9c8236ae4ae89f356
Rebecca Sissman <rsissman15@aol.com>
2022-02-14 10:50:58 -0500

    Completed Assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code here
+class Breakfast{
+    constructor(food,drink){
+        this.food=food
+        this.drink=drink
+    }
+}
+
+let newBreakfast=new Breakfast('cereal','orange juice');
+
+class Lunch{
+    constructor(salad,soup,drink){
+        this.salad=salad
+        this.soup=soup
+        this.drink=drink;
+    }
+}
+
+let newLunch= new Lunch('romaine','chicken noodle','water');
+
+
+class Dinner{
+    #dessert;
+    constructor(salad,soup,entree,dessert){
+        this.salad=salad
+        this.soup=soup
+        this.entree=entree
+        this.#dessert=dessert
+    }
+}
+
+let newDinner=new Dinner ('cobb','french onion','chicken','cake')
